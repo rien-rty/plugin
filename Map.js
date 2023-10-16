8b0ee4ce5b00c76b01be457153b360707e8205cf
Rien <rien.boydens@routeyou.com>
2023-10-16 13:27:03 +0200

    Added a Map

---
+++ Map.js
@@ -0,0 +1,12 @@
+'use strict'
+
+/**
+ * Creates a Map component
+ *
+ * @constructor
+ * @param {!Elemen} element
+ * @return undefined
+ */
+RTY.Plugin.Map = function(element) {
+    this._element = element;
+}
